2da4166dff4fba530a15131808e07a3a17cc417c
Ye Wenbin <wenbinye@gmail.com>
2020-11-11 19:32:31 +0800

    add command to check schema

--- src/commands/SyncCommand.php
+++ src/commands/SyncCommand.php
@@ -1,16 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
 namespace winwin\db\tools\commands;
 
+use Doctrine\DBAL\Schema\Comparator;
+use PDOException;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use winwin\db\tools\schema\Schema;
-use kuiper\helper\DataDumper;
-use Doctrine\DBAL\Schema\Comparator;
-use SqlFormatter;
-use PDOException;
 
 class SyncCommand extends BaseSchemaCommand
 {
@@ -18,9 +18,10 @@ class SyncCommand extends BaseSchemaCommand
     {
         parent::configure();
         $this->setName('sync')
-            ->setDescription("Update database schema")
-            ->addOption('purge', null, InputOption::VALUE_NONE, "Remove not present tables")
-            ->addOption('target', '-t', InputOption::VALUE_REQUIRED, "Database schema diff to");
+            ->setDescription('Update database schema')
+            ->addOption('purge', null, InputOption::VALUE_NONE, 'Remove not present tables')
+            ->addOption('check', null, InputOption::VALUE_NONE, 'Exit abnormal if changed')
+            ->addOption('target', '-t', InputOption::VALUE_REQUIRED, 'Database schema diff to');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -41,14 +42,20 @@ class SyncCommand extends BaseSchemaCommand
         $comparator = new Comparator();
         $diff = $comparator->compare($currentSchema, $toSchema);
         $sqls = Schema::toSql($diff, $db = $this->getConnection($input));
-        
+
         if (empty($sqls)) {
-            $output->writeln("<info>No changes</info>");
+            $output->writeln('<info>No changes</info>');
+
             return 0;
-        } 
+        }
         $sql = $this->formatSql($sqls);
+        if ($input->getOption('check')) {
+            $output->writeln("Sql to change: \n".$sql);
+
+            return 1;
+        }
         if ($input->isInteractive()) {
-            $helper = $this->getHelper("question");
+            $helper = $this->getHelper('question');
             $question = new ConfirmationQuestion("{$sql}\nContinue execute above sql? (y/n) [n] ", false);
             if (!$helper->ask($input, $output, $question)) {
                 return 0;
@@ -62,9 +69,10 @@ class SyncCommand extends BaseSchemaCommand
                     $output->writeln(sprintf("<error>Fail to execute '%s': %s</error>", $stmt, json_encode($errorInfo)));
                 }
             } catch (PDOException $e) {
-                $output->writeln(sprintf("<error>Fail to execute '%s': %s</error>", $stmt, $e->getMessage() . " " . json_encode($e->errorInfo)));
+                $output->writeln(sprintf("<error>Fail to execute '%s': %s</error>", $stmt, $e->getMessage().' '.json_encode($e->errorInfo)));
             }
         }
+
         return 0;
     }
 }
